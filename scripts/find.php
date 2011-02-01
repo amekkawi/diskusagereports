fa00c61b293f725cd39427beb8cb4dad5b214249
Andre Mekkawi <github@andremekkawi.com>
2011-02-01 22:35:10 +0000

    Updated help in find.php to point to online docs for now.

--- scripts/find.php
+++ scripts/find.php
@@ -15,7 +15,7 @@ $args = array(
 );
 
 $cliargs = array_slice($_SERVER['argv'], 1);
-$syntax = "Syntax: php find.php [options] <directory>\nUse -? for help.\n";
+$syntax = "Syntax: php find.php [options] <directory>\nSee http://diskusagereport.sourceforge.net/docs/index.html for help.\n";
 
 while (!is_null($cliarg = array_shift($cliargs))) {
 	$shifted = true;
