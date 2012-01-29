249da2ac966b94632505ef7dc9e524d102783ff9
Andre Mekkawi <github@andremekkawi.com>
2012-01-29 20:03:59 +0000

    * Created basic scripts/inc/process.class.php

---
+++ scripts/inc/process.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * Copyright (c) 2011 André Mekkawi <contact@andremekkawi.com>
+ * Revision: $Revision$
+ * 
+ * LICENSE
+ * 
+ * This source file is subject to the MIT license in the file LICENSE.txt.
+ * The license is also available at http://diskusagereport.sf.net/license.html
+ */
+
+/**
+ * Process the output of find.php or find.sh and create static report files.
+ * 
+ */
+class Process {
+	
+}
+?>
