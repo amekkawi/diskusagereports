04bad02eab1ecf65f9801cd52c026dd276b6999a
Andre Mekkawi <github@andremekkawi.com>
2012-03-20 23:41:55 -0400

    Further site improvements.

--- js/docs.js
+++ js/docs.js
@@ -1,19 +1,39 @@
 ;(function($) {
 	$(function(){
 		var win = $(window),
+			doc = $('#Docs'),
 			toc = $('#TOC'),
 			tocTop = toc.size() && toc.offset().top - 20,
-			isFixed = 0;
+			tocHeight = toc.size() && toc.outerHeight(true) + 25,
+			docTop = doc.offset().top,
+			docHeight = doc.height(),
+			isFixed = 0,
+			isBottom = 0;
 		
 		var processScroll = function() {
-			var i, scrollTop = win.scrollTop();
-			if (scrollTop >= tocTop && !isFixed) {
-				isFixed = 1;
-				toc.addClass('fixed');
-			}
-			else if (scrollTop <= tocTop && isFixed) {
+			var i, scrollTop = win.scrollTop(),
+				isShort = docHeight + docTop - scrollTop <= tocHeight,
+				viewPort = win.height();
+				
+			if (viewPort < tocHeight) {
 				isFixed = 0;
-				toc.removeClass('fixed');
+				isBottom = 0;
+				toc.removeClass('fixed bottom');
+			}
+			else if (isShort && !isBottom) {
+				isBottom = 1;
+				isFixed = 0;
+				toc.addClass('bottom').removeClass('fixed');
+			}
+			else if (!isShort && scrollTop >= tocTop && !isFixed) {
+				isFixed = 1;
+				isBottom = 0;
+				toc.addClass('fixed').removeClass('bottom');
+			}
+			else if (scrollTop <= tocTop && (isFixed || isBottom)) {
+				isFixed = 0;
+				isBottom = 0;
+				toc.removeClass('fixed bottom');
 			}
 		};
 		
