c7f502b89a4679b7376e75a6bcbfd9f202d9ea27
Andre Mekkawi <github@andremekkawi.com>
2012-04-02 15:24:05 -0400

    Changed JS packing so that everything is in one file.

--- $dev/inc/prepare.sed
+++ $dev/inc/prepare.sed
@@ -1,9 +1,8 @@
 # Comment out all <script> tags.
 s#^<script type="text/javascript" src="js/.+"></script>$#<!-- & -->#
 
-# Uncomment the packed.js and jquery.js <script> tags.
+# Uncomment the packed.js <script> tag.
 s#^<!-- (<script type="text/javascript" src="js/packed\.js"></script>) -->$#\1#
-s#^<!-- (<script type="text/javascript" src="js/external/jquery\.js"></script>) -->$#\1#
 
 # Use jquery.min.js instead.
-s#"js/external/jquery.js"#"js/external/jquery-min.js"#
+s#"js/external/jquery.js"#"js/external/jquery.min.js"#
--- $dev/pack.sh
+++ $dev/pack.sh
@@ -2,20 +2,30 @@
 
 DEV=$(cd "$(dirname "$0")"; pwd)
 CODEBASE=$(dirname "$DEV")
+SED='s/^.*<script .+ src="([^"]+)".*$/\1/p'
 
 cd "$CODEBASE"
 
-sed -nE -e 's/^.*<script .+ src="([^"]+)".*$/\1/p' "$CODEBASE/index.html" \
-	| grep -v 'packed.js' | grep -v '.min.js' \
-	| xargs cat \
-	| php "$DEV/inc/packer-stdin.php" \
-	> "$CODEBASE/js/packed.js"
+echo "
+// Packed version of .js scripts in index.html
+// See original files for copyright info.
+" > "js/packed.js"
 
-packed=$(cat "$CODEBASE/js/packed.js" | wc -c | tr -d ' ')
+while IFS="" read file; do
 	
-unpacked=$(sed -nE -e 's/^.*<script .+ src="([^"]+)".*$/\1/p' "$CODEBASE/index.html" \
-	| grep -v 'packed.js' | grep -v '.min.js' \
-	| xargs cat | wc -c | tr -d ' ')
+	[ ! -f "$file" ] && echo "File not found: $file" && exit 1
+	
+	echo "// Packed $file" >> "js/packed.js"
+	cat "$file" | php "$DEV/inc/packer-stdin.php" >> "js/packed.js"
+	
+	echo >> "js/packed.js"
+	echo >> "js/packed.js"
+done < <(sed -nE -e "$SED" "index.html" | grep -v 'packed.js')
+
+packed=$(cat "js/packed.js" | wc -c | tr -d ' ')
+	
+unpacked=$(sed -nE -e "$SED" "index.html" \
+	| grep -v 'packed.js' | xargs cat | wc -c | tr -d ' ')
 
 percent=$(echo "scale=2; $packed * 100 / $unpacked" | bc)
 
