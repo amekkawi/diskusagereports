474af73c1d4c4457a63eecb224176216c8d1f434
Andre Mekkawi <github@andremekkawi.com>
2012-07-06 19:23:58 -0400

    find.sh now handles decimals after the seconds part of the timestamp.

--- scripts/find.sh
+++ scripts/find.sh
@@ -17,7 +17,7 @@ export LC_ALL=C
 function determine_format() {
 	# Check if the find commands supports -printf and -mindepth
 	line="$(find "$0" -mindepth 0 -printf "%y %TY-%Tm-%Td %TH:%TM:%TS %s %P\n" 2> /dev/null)"
-	[ "$?" == "0" ] && echo "$line" | grep -Eq '^. [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]+ $' \
+	[ "$?" == "0" ] && echo "$line" | grep -Eq '^. [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)? [0-9]+ $' \
 		&& mode="find-printf" && format="timestamp" && return 0
 	
 	# Make sure find supports -print0
@@ -51,17 +51,17 @@ function determine_format() {
 	
 	# Check if --full-time is available.
 	line="$(ls -ld --full-time . 2> /dev/null)"
-	[ "$?" == "0" ] && echo "$line" | awk '{ print $6, $7, $8, $9 }' | grep -Eq '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]+ \+0000 \.$' \
+	[ "$?" == "0" ] && echo "$line" | awk '{ print $6, substr($7, 0, 8), $8, $9 }' | grep -Eq '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \+0000 \.$' \
 		&& awkarg='{ printf "%s%c%s%c%s%c%s%c%s\n", substr($1, 0, 1), '$delimdec', $6, '$delimdec', substr($7, 0, 8), '$delimdec', $5, '$delimdec', substr($0, index($0, " " $8 " " $9) + length($8) + 4, 1024) }' \
 		&& mode="ls-awk" && format="timestamp" && lsarg="--full-time" && return 0
 	
 	# Check if -T for displaying full date/time is available.
 	line="$(ls -ldT . 2> /dev/null)"
-	[ "$?" == "0" ] && echo "$line" | awk '{ print $6, $7, $8, $9, $10 }' | grep -Eq '^[A-Z][a-z]{2} [0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4} \.$' \
-		&& awkarg='BEGIN { split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec", month, " "); for (i=1; i<=12; i++) mdigit[month[i]] = sprintf("%02d", i) }; { printf "%s%c%s%c%s%c%s%c%s\n", substr($1, 0, 1), '$delimdec', $9 "-" mdigit[$6] "-" sprintf("%02d", $7), '$delimdec', $8, '$delimdec', $5, '$delimdec', substr($0, index($0, " " $8 " " $9 " ") + 17, 1024) }' \
+	[ "$?" == "0" ] && echo "$line" | awk '{ print $6, $7, substr($8, 0, 8), $9, $10 }' | grep -Eq '^[A-Z][a-z]{2} [0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4} \.$' \
+		&& awkarg='BEGIN { split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec", month, " "); for (i=1; i<=12; i++) mdigit[month[i]] = sprintf("%02d", i) }; { printf "%s%c%s%c%s%c%s%c%s\n", substr($1, 0, 1), '$delimdec', $9 "-" mdigit[$6] "-" sprintf("%02d", $7), '$delimdec', substr($8, 0, 8), '$delimdec', $5, '$delimdec', substr($0, index($0, " " $8 " " $9 " ") + 17, 1024) }' \
 		&& mode="ls-awk" && format="timestamp" && lsarg="-T" && return 0
 	
-	# // TODO: Possibly allow the default format since we include current date/time? Will not be able to determine time however.
+	# // TODO: Possibly allow the default format since we include current date/time as reference? Will not be able to determine exact time for most files however.
 	
 	return 200
 }
