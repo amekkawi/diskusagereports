677b8767123397e9230e17bec7c3d92e57b8e694
Andre Mekkawi <github@andremekkawi.com>
2012-09-12 20:43:37 -0400

    Fixes to define logic in stdafx.h

--- find/stdafx.h
+++ find/stdafx.h
@@ -16,21 +16,27 @@
 #ifndef _TSTRING
 	#ifdef _UNICODE
 		#define _TSTRING wstring
-		#define _TMAX_PATH 32767
-
-		#define _TMAX_DRIVE 32767
-		#define _TMAX_DIR 32767
-		#define _TMAX_FNAME 32767
-		#define _TMAX_EXT 32767
 	#else
 		#define _TSTRING string
-		#define _TMAX_PATH MAX_PATH
-
-		#define _TMAX_DRIVE _MAX_DRIVE
-		#define _TMAX_DIR _MAX_DIR
-		#define _TMAX_FNAME _MAX_FNAME
-		#define _TMAX_EXT _MAX_EXT
 	#endif
+#endif
+
+#ifdef _UNICODE
+	#define _ISUNICODE true
+	#define _TMAX_PATH 32767
+
+	#define _TMAX_DRIVE 32767
+	#define _TMAX_DIR 32767
+	#define _TMAX_FNAME 32767
+	#define _TMAX_EXT 32767
+#else
+	#define _ISUNICODE false
+	#define _TMAX_PATH MAX_PATH
+
+	#define _TMAX_DRIVE _MAX_DRIVE
+	#define _TMAX_DIR _MAX_DIR
+	#define _TMAX_FNAME _MAX_FNAME
+	#define _TMAX_EXT _MAX_EXT
 #endif
 
 // TODO: reference additional headers your program requires here
