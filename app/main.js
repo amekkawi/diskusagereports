f525f98ec4be063b353f9e810bd15d7e32e428ac
Andre Mekkawi <github@andremekkawi.com>
2013-04-12 22:34:42 -0400

    Added license to JS files and updated license in index.html.

--- app/main.js
+++ app/main.js
@@ -1,3 +1,11 @@
+/*
+ * Disk Usage Reports
+ * http://diskusagereports.com/
+ *
+ * Copyright (c) 2013 André Mekkawi <diskusage@andremekkawi.com>
+ * This source file is subject to the MIT license in the file LICENSE.txt.
+ * The license is also available at http://diskusagereports.com/license.html
+ */
 require([
 	
 ],
