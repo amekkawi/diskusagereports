2497f949461121db5743abb2486b1c1a665fa4d4
Andre Mekkawi <github@andremekkawi.com>
2013-04-14 16:16:41 -0400

    Added LESS processor to grunt-htmlbuild.

--- grunt/grunt-htmlbuild.js
+++ grunt/grunt-htmlbuild.js
@@ -247,6 +247,66 @@ module.exports = function(grunt) {
 				}, this);
 
 				return outTags;
+			},
+
+			less: function(opts) {
+				var syntax = 'Syntax: <dest>',
+					args = opts.args;
+
+				if (!_.isString(args))
+					grunt.fail.warn('Missing arguments. ' + syntax);
+
+				var contents = opts.contents,
+					target = this.options.target,
+					outTags = [],
+					splitArgs = args.split(/[ \t]+/);
+
+				// Parse Arguments
+
+				if (splitArgs.length < 1)
+					grunt.fail.warn('Missing arguments. ' + syntax);
+
+				var dest = { short: splitArgs[0], full: path.join(this.options.baseUrl, splitArgs[0]) };
+				grunt.event.emit(this.task.name + '.notice', { verbose: true, message: "Set destination to " + dest.full });
+				splitArgs.shift();
+
+				_.each(this.getTags('link', contents), function(tag){
+					grunt.event.emit(this.task.name + '.notice', { verbose: true, message: "Parsing tag: " + tag._html });
+
+					if (!_.isString(tag.href)) {
+						grunt.fail.warn("Tag missing href attribute: " + tag._html);
+						return false;
+					}
+
+					if (!_.isString(tag.rel)) {
+						grunt.fail.warn("Tag missing rel attribute: " + tag._html);
+						return false;
+					}
+
+					if (!_.isString(tag.type)) {
+						grunt.fail.warn("Tag missing type attribute: " + tag._html);
+						return false;
+					}
+
+					if (tag.type != "text/css") {
+						grunt.fail.warn("Tag's type attribute is not 'text/css': " + tag._html);
+						return false;
+					}
+
+					if (!grunt.file.isFile(tag.href)) {
+						grunt.fail.warn("Cannot find file for href: " + tag._html);
+						return false;
+					}
+
+					if (!outTags.length) {
+						outTags.push('<link rel="stylesheet" type="text/css" href="' + dest.short + '">');
+						grunt.event.emit(this.task.name + '.notice', { verbose: true, message: "Added tag: " + outTags[outTags.length - 1] });
+					}
+
+					grunt.event.emit(this.task.name + '.less', { target: target, src: tag.href, dest: dest.full });
+				}, this);
+
+				return outTags;
 			}
 		}
 	});
@@ -287,8 +347,10 @@ module.exports = function(grunt) {
 						concat = grunt.config(options.concat || 'concat'),
 						uglify = grunt.config(options.uglify || 'uglify'),
 						requirejs = grunt.config(options.requirejs || 'requirejs'),
+						less = grunt.config(options.less || 'less'),
 						concatDests,
-						uglifyDests;
+						uglifyDests,
+						lessDests;
 
 					grunt.event.on(this.name + '.blockfound', function(ev) {
 						grunt.log.subhead("Block found: " + grunt.log.wordlist([
@@ -297,6 +359,7 @@ module.exports = function(grunt) {
 
 						concatDests = {};
 						uglifyDests = {};
+						lessDests = {};
 					});
 
 					grunt.event.on(this.name + '.notice', function(ev) {
@@ -327,6 +390,19 @@ module.exports = function(grunt) {
 							uglifyDests[ev.target][ev.dest] = initFilesDest(uglify, ev.target, ev.dest);
 
 						uglifyDests[ev.target][ev.dest].push(ev.src);
+					});
+
+					grunt.event.on(this.name + '.less', function(ev) {
+						grunt.log.writeln("Added less:" + ev.target + " " + grunt.log.wordlist([ ev.src, ev.dest ], { separator: ' -> ' }));
+						configChanged['less'] = less;
+
+						if (!lessDests[ev.target])
+							lessDests[ev.target] = {};
+
+						if (!lessDests[ev.target][ev.dest])
+							lessDests[ev.target][ev.dest] = initFilesDest(less, ev.target, ev.dest);
+
+						lessDests[ev.target][ev.dest].push(ev.src);
 					});
 
 					grunt.event.on(this.name + '.requirejs', function(ev) {
