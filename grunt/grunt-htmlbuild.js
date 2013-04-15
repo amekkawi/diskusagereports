0ceb7dc9b65a3c87d2a531e5112dddeb128d263c
Andre Mekkawi <github@andremekkawi.com>
2013-04-15 17:46:06 -0400

    Changed the requirejs parser in grunt-htmlbuild so it allows for a JSON arg that will override values in the requirejs task's options.

--- grunt/grunt-htmlbuild.js
+++ grunt/grunt-htmlbuild.js
@@ -178,7 +178,7 @@ module.exports = function(grunt) {
 
 		typeParser: {
 			requirejs: function(opts) {
-				var syntax = 'Syntax: <data-main> [<dest> [<target>]]',
+				var syntax = 'Syntax: <data-main> [<dest> [<target>]] [<options-json>]',
 					args = opts.args;
 
 				if (!_.isString(args))
@@ -186,26 +186,43 @@ module.exports = function(grunt) {
 
 				var target = this.options.target,
 					outTags = [],
-					splitArgs = args.split(/[ \t]+/);
+					splitArgs = args.replace(/^[ \t]+/, '').replace(/[ \t]+$/, '').split(/ /);
 
 				// Parse Arguments
 
 				if (splitArgs.length < 1)
 					grunt.fail.warn('Missing arguments. ' + syntax);
 
+				// <data-main>
 				var main = splitArgs.shift();
 				grunt.event.emit(this.task.name + '.notice', { verbose: true, message: "Set main to " + main });
+				splitArgs = splitArgs.length ? splitArgs.join(' ').replace(/^[ \t]+/, '').split(/ /) : splitArgs;
 
+				// <dest>
 				var dest = { short: main + '.js', full: path.join(this.options.baseUrl, main + '.js') };
-				if (splitArgs.length) {
+				if (splitArgs.length && !splitArgs[0].match(/^[ \t]*{/)) {
 					dest = { short: splitArgs[0], full: path.join(this.options.baseUrl, splitArgs[0]) };
 					grunt.event.emit(this.task.name + '.notice', { verbose: true, message: "Set destination to " + dest.full });
 					splitArgs.shift();
+					splitArgs = splitArgs.length ? splitArgs.join(' ').replace(/^[ \t]+/, '').split(/ /) : splitArgs;
 				}
 
-				if (splitArgs.length) {
+				// <target>
+				if (splitArgs.length && !splitArgs[0].match(/^[ \t]*{/)) {
 					target = splitArgs.shift();
 					grunt.event.emit(this.task.name + '.notice', { verbose: true, message: "Set target to " + target });
+					splitArgs = splitArgs.length ? splitArgs.join(' ').replace(/^[ \t]+/, '').split(/ /) : splitArgs;
+				}
+
+				// <options>
+				var argOptions = {};
+				if (splitArgs.length && splitArgs[0].match(/^[ \t]*{/)) {
+					try {
+						argOptions = JSON.parse(splitArgs.join(' '));
+					}
+					catch (e) {
+						grunt.fail.warn('Invalid JSON (' + e + '): ' + splitArgs.join(' '));
+					}
 				}
 
 				// Process tags.
@@ -217,12 +234,12 @@ module.exports = function(grunt) {
 					target: target,
 					src: main + '.js',
 					dest: dest.full,
-					options: {
+					options: _.extend({
 						baseUrl: path.dirname(main),
 						name: path.basename(main),
 						out: dest.full,
 						mainConfigFile: main + '.js'
-					}
+					}, argOptions)
 				});
 
 				grunt.event.emit(this.task.name + '.uglify', { target: target, src: dest.full, dest: dest.full });
